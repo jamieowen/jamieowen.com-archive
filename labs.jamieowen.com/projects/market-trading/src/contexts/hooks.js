502fbb0460ea6fbe0f75d4dfea4512604ba86998
Jamie Owen <jamie.owen@lloydsbanking.com>
2019-11-13 17:00:07 +0000

    crypto idea - forget about for now

---
+++ labs.jamieowen.com/projects/market-trading/src/contexts/hooks.js
@@ -0,0 +1,19 @@
+import {
+  useReducer,
+  useMemo,
+  useState
+} from 'react';
+
+const useExchangeProvider = (exchange)=>{
+
+}
+
+const useMarketProvider = (exchange)=>{
+
+}
+
+
+export {
+  useExchangeProvider,
+  useMarketProvider
+}
