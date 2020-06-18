37b39c6b8b430f61e0d9d6adba63063a5b60a92e
Jamie Owen <hello@jamieowen.com>
2020-06-18 20:31:30 +0100

    remove old traefik and jupyter setup

--- setup-server.sh
+++
@@ -1,8 +0,0 @@
-# docker build -t jamieowen.com ./jamieowen.com
-# docker build -t labs.jamieowen.com ./labs.jamieowen.com
-
-docker-compose -f docker-compose-setup.yml up -d
-docker network create web
-
-touch acme.json
-chmod 600 acme.json
